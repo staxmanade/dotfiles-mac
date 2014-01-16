9ef16c70187ea41701d60aa4d47f02195060e250
Matt Carrier <icecreammatt@gmail.com>
2014-01-15 20:30:53 -0800

    Moved config files into subdirectory to make adding new ones easier

--- README.md
+++ README.md
@@ -2,3 +2,9 @@ dotfiles
 ========
 
 # Setup
+
+```
+git clone git@bitbucket.org:icecreammatt/student-dotfiles.git dotfiles
+cd dotfiles
+./setup.sh
+```
--- setup_links.sh
+++ setup_links.sh
@@ -10,13 +10,14 @@ backupDirectory=.dotfiles_backup_$date
 mkdir $HOME/$backupDirectory 2> /dev/null
 
 #List of links to create
-files=(vimrc emacs bash_profile bashrc gitconfig gitignore_global tmux.conf git-prompt.sh)
+files=$(ls $HOME/dotfiles/configs)
 
 # For each link in files backup the existing one if there is one and then link to the new one
 for linkName in ${files[@]}
 do
+       echo $linkName
        mv $HOME/.$linkName $HOME/$backupDirectory 2> /dev/null
-       ln -s $HOME/dotfiles/$linkName $HOME/.$linkName
+       ln -s $HOME/dotfiles/configs/$linkName $HOME/.$linkName
 done
 
 mv $HOME/.vim $HOME/$backupDirectory 2> /dev/null
